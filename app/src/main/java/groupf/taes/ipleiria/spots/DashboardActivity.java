7a719bc7686abccda5f1cd598f391ea92043c88e
Amanda <2160832@my.ipleiria.pt>
2018-11-03 17:24:06 +0000

    US1 mapa já está a funcionar com action bar Falta estacionamentos

--- app/src/main/java/groupf/taes/ipleiria/spots/DashboardActivity.java
+++ app/src/main/java/groupf/taes/ipleiria/spots/DashboardActivity.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DashboardActivity extends FragmentActivity /*AppCompatActivity*/ implements OnMapReadyCallback {
+public class DashboardActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
