b20406f5455939f5edb66bf4b704f78ffb4d9075
Amanda <2160832@my.ipleiria.pt>
2018-11-03 22:53:31 +0000

    Método que pega os dados relacionados às vagas de estacionamento já implementado

--- app/src/main/java/modelo/SpotsManager.java
+++ app/src/main/java/modelo/SpotsManager.java
@@ -2,23 +2,28 @@ package modelo;
 
 import android.provider.ContactsContract;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class SpotsManager {
     private static final SpotsManager INSTANCE = new SpotsManager();
+    private FirebaseDatabase database;
     private DatabaseReference dbRef;
     private List<Spot> spots;
 
     public SpotsManager() {
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        dbRef = database.getReference();
+        database = FirebaseDatabase.getInstance();
+        dbRef = database.getReference().child("ParkingSpots");
         spots = new LinkedList<>();
-        writeSpotsOnDatabase();
+       // writeSpotsOnDatabase();
+        readSpotsDataFromDatabase();
     }
 
     public void writeSpotsOnDatabase() {
@@ -28,10 +33,37 @@ public class SpotsManager {
         spots.add(new Spot("A-4", "A", "39,734905,-8.820718", 1));
 
         for(Spot s : spots) {
-            dbRef.child("ParkingSpots").child(s.getSpotId()).child("Park").setValue(s.getPark());
-            dbRef.child("ParkingSpots").child(s.getSpotId()).child("LocationGeo").setValue(s.getLocationGeo());
-            dbRef.child("ParkingSpots").child(s.getSpotId()).child("Status").setValue(s.getStatus());
+           // dbRef.child("ParkingSpots").child(s.getSpotId()).child("Park").setValue(s.getPark());
+            dbRef.child(s.getSpotId()).child("Park").setValue(s.getPark());
+            dbRef.child(s.getSpotId()).child("LocationGeo").setValue(s.getLocationGeo());
+            dbRef.child(s.getSpotId()).child("Status").setValue(s.getStatus());
         }
+    }
+
+    public void readSpotsDataFromDatabase() {
+
+        DatabaseReference ref = database.getReference().child("ParkingSpots");
+
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                for (DataSnapshot d : children) {
+                    Spot spot = new Spot(d.getKey(), d.child("Park").getValue().toString(), d.child("LocationGeo").getValue().toString(), Integer.parseInt(d.child("Status").getValue().toString()));
+                    spots.add(spot);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+
+
     }
 
     public static SpotsManager getINSTANCE() {
