eb41f703538d4ac69f9b36aaa5b569d65982fcd4
Amanda <2160832@my.ipleiria.pt>
2018-11-03 16:54:15 +0000

    US1 parte da implementação quase feito

--- app/src/main/java/groupf/taes/ipleiria/spots/DashboardActivity.java
+++ app/src/main/java/groupf/taes/ipleiria/spots/DashboardActivity.java
@@ -1,15 +1,45 @@
 package groupf.taes.ipleiria.spots;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DashboardActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DashboardActivity extends FragmentActivity /*AppCompatActivity*/ implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        //getActionBar().show();
 
-        //Test
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+       /* LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)); */
+
+        LatLng ParqueA = new LatLng(39.734994,  -8.820697);
+        mMap.addMarker(new MarkerOptions().position(ParqueA).title("Parque Estacionamento A"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ParqueA));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    }
+
+
 }
