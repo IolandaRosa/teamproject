bfb9711c7551a057ac172d29aac6bc144909fb38
Amanda <2160832@my.ipleiria.pt>
2018-11-03 19:16:18 +0000

    Já são colocados os markers

--- app/src/main/java/groupf/taes/ipleiria/spots/DashboardActivity.java
+++ app/src/main/java/groupf/taes/ipleiria/spots/DashboardActivity.java
@@ -1,8 +1,9 @@
 package groupf.taes.ipleiria.spots;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +11,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import modelo.Spot;
+import modelo.SpotsManager;
 
 public class DashboardActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -18,8 +24,9 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SpotsManager.getINSTANCE();
         setContentView(R.layout.activity_dashboard);
-        //getActionBar().show();
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapFragment);
@@ -30,16 +37,21 @@ public class DashboardActivity extends AppCompatActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-       /* LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)); */
 
-        LatLng ParqueA = new LatLng(39.734994,  -8.820697);
-        mMap.addMarker(new MarkerOptions().position(ParqueA).title("Parque Estacionamento A"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ParqueA));
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        for(Spot s : SpotsManager.getINSTANCE().getSpots()) {
+            String location = s.getLocationGeo();
+            String[] geo = location.split(",");
+           /* AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage(geo[0] + " - " + geo[1]); */
+            //Toast.makeText(this, geo[0] + " - " + geo[1], Toast.LENGTH_LONG);
+            LatLng marker = new LatLng(Float.parseFloat(geo[0]), Float.parseFloat(geo[1]));
+            mMap.addMarker(new MarkerOptions().position(marker).title(s.getSpotId()));
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+
+
     }
+
 
 
 }
---
+++ app/src/main/java/modelo/SpotsManager.java
@@ -0,0 +1,44 @@
+package modelo;
+
+import android.provider.ContactsContract;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpotsManager {
+    private static final SpotsManager INSTANCE = new SpotsManager();
+    private DatabaseReference dbRef;
+    private List<Spot> spots;
+
+    public SpotsManager() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        dbRef = database.getReference();
+        spots = new LinkedList<>();
+        writeSpotsOnDatabase();
+    }
+
+    public void writeSpotsOnDatabase() {
+        spots.add(new Spot("A-1", "A", "39.734859,-8.820784", 0));
+        spots.add(new Spot("A-2", "A", "39.734884,-8.820745", 0));
+        spots.add(new Spot("A-3", "A", "39.734909,-8.820708", 0));
+        spots.add(new Spot("A-4", "A", "39,734905,-8.820718", 0));
+
+        for(Spot s : spots) {
+            dbRef.child("ParkingSpots").child(s.getSpotId()).child("Park").setValue(s.getPark());
+            dbRef.child("ParkingSpots").child(s.getSpotId()).child("LocationGeo").setValue(s.getLocationGeo());
+            dbRef.child("ParkingSpots").child(s.getSpotId()).child("Status").setValue(s.getStatus());
+        }
+    }
+
+    public static SpotsManager getINSTANCE() {
+        return INSTANCE;
+    }
+
+    public List<Spot> getSpots() {
+        return spots;
+    }
+}
