ef9a9b96aeae9999aabd8575eff4952259ba2245
AmandaVieiraP <2160832@my.ipleiria.pt>
2018-12-22 00:51:24 +0000

    GROUPF-113 a fazer testes #done

--- app/src/androidTest/java/features/US18FeatureTest.java
+++ app/src/androidTest/java/features/US18FeatureTest.java
@@ -1,14 +1,114 @@
 package features;
 
+import android.support.test.rule.ActivityTestRule;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mauriciotogneri.greencoffee.GreenCoffeeConfig;
 import com.mauriciotogneri.greencoffee.GreenCoffeeTest;
 import com.mauriciotogneri.greencoffee.ScenarioConfig;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import groupf.taes.ipleiria.spots.DashboardAuthActivity;
+import modelo.SpotsManager;
+import modelo.UsersManager;
+import steps.US18FeatureSteps;
+
+@RunWith(Parameterized.class)
 public class US18FeatureTest extends GreenCoffeeTest {
 
-    
+    @Rule
+    public ActivityTestRule activityTestRule = new ActivityTestRule(DashboardAuthActivity.class);
+
     public US18FeatureTest(ScenarioConfig scenario) {
         super(scenario);
     }
+
+    @Test
+    public void test() {
+        start(new US18FeatureSteps());
+    }
+
+    @Parameterized.Parameters (name = "{0}")
+    public static Collection<ScenarioConfig> data() throws IOException {
+        return new GreenCoffeeConfig().withFeatureFromAssets("assets/features/featureUS18.feature").scenarios();
+    }
+
+    @BeforeClass
+    public static void setUpOnlyOnce() throws Exception {
+
+        SpotsManager.INSTANCE.addSpotToDatabase("TestSpot", "A", "39.735008,-8.820593", 1, 0);
+
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null)
+            FirebaseAuth.getInstance().signOut();
+
+        //regista o utilizador
+        Task<AuthResult> registerTask = UsersManager.INSTANCE.registerUser("spots7@email.pt", "12345678");
+
+        //todo - não é a melhor solução mas em termos de performance é melhor que sleep
+        while(!registerTask.isComplete())
+            Thread.sleep(1);
+
+
+        if(registerTask.isSuccessful()){
+            //Coloca utilizador na BD sem spots
+            UsersManager.INSTANCE.addUserThatIsParked("Spots","spots7@email.pt", "TestSpot");
+        }
+        else{
+            Task<AuthResult> loginTask = UsersManager.INSTANCE.makeLogin("spots7@email.pt", "12345678");
+
+            //todo - não é a melhor solução mas em termos de performance é melhor que sleep
+            while(!loginTask.isComplete())
+                Thread.sleep(1);
+
+            //Coloca utilizador na BD sem spots
+            UsersManager.INSTANCE.addUserThatIsParked("Spots","spots7@email.pt", "TestSpot");
+        }
+
+    }
+
+
+    @AfterClass
+    public static void tearDownOnlyOnce() throws Throwable {
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null) {
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            String uid = currentUser.getUid();
+            currentUser.delete();
+
+            FirebaseDatabase.getInstance().getReference("users").child(uid).removeValue();
+        }else{
+            //Se não fazer login - não deve acontecer em principio ele esta logado sempre - e eliminar
+            Task<AuthResult> loginTask = UsersManager.INSTANCE.makeLogin("spots3@email.pt", "12345678");
+
+            //todo - não é a melhor solução mas em termos de performance é melhor que sleep
+            while(!loginTask.isComplete())
+                Thread.sleep(1);
+
+            if(loginTask.isSuccessful()){
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = currentUser.getUid();
+                currentUser.delete();
+
+                FirebaseDatabase.getInstance().getReference("users").child(uid).removeValue();
+
+            }
+        }
+
+        SpotsManager.INSTANCE.removeSpotFromDatabase("TestSpot");
+    }
+
 
 
 }
--- app/src/androidTest/java/steps/US18FeatureSteps.java
+++ app/src/androidTest/java/steps/US18FeatureSteps.java
@@ -1,34 +1,85 @@
 package steps;
 
+import android.support.test.espresso.Espresso;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.mauriciotogneri.greencoffee.GreenCoffeeSteps;
 import com.mauriciotogneri.greencoffee.annotations.Given;
 import com.mauriciotogneri.greencoffee.annotations.Then;
+
+import org.junit.Assert;
+
+import groupf.taes.ipleiria.spots.R;
+import helpers.DrawerHelper;
+import modelo.Spot;
+import modelo.SpotsManager;
+import modelo.UsersManager;
+
+import static android.os.SystemClock.sleep;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class US18FeatureSteps extends GreenCoffeeSteps {
 
     @Given("^I am an authenticated user$")
     public void i_am_an_authenticated_user() {
-
+        Assert.assertNotNull(FirebaseAuth.getInstance().getCurrentUser());
     }
 
     @Given("^I am parked$")
     public void i_am_parked() {
+        Assert.assertNotNull(UsersManager.INSTANCE.getCurrentUser().getSpotParked());
     }
 
     @Given("^I select the option \"([^\"]*)\" on the menu$")
     public void i_select_the_option_on_the_menu(String arg1) {
+        onViewWithId(R.id.drawer_layout).isDisplayed();
+        Espresso.onView(withId(R.id.drawer_layout)).perform(DrawerHelper.actionOpenDrawer());
+        sleep(1000);
+        Espresso.onView(withText(arg1)).perform(click());
+    }
 
+    @Given("^I see a dialog asking if I want to leave$")
+    public void i_see_a_dialog_asking_if_I_want_to_leave() {
+        onViewWithText(string(R.string.askUserIsLeavingTheSpot)).isDisplayed();
+    }
+
+    @Given("^I select the option \"([^\"]*)\"$")
+    public void i_select_the_option(String arg1) {
+        onViewWithText(arg1).click();
     }
 
     @Given("^I see an ecran where I can rate the spot, add to my favourites and close this ecran$")
     public void i_see_an_ecran_where_I_can_rate_the_spot_add_to_my_favourites_and_close_this_ecran() {
-
+        onViewWithId(R.id.ratingBar).isDisplayed();
+        onViewWithId(R.id.btnSendRate).isDisplayed();
+        onViewWithId(R.id.btnAddToFavourites).isDisplayed();
+        onViewWithId(R.id.btnClose).isDisplayed();
     }
 
     @Then("^I press the close button$")
     public void i_press_the_close_button() {
-
+        onViewWithId(R.id.btnClose).click();
     }
+
+    @Then("^I am on the auth dashboard$")
+    public void i_am_on_the_auth_dashboard() {
+        onViewWithId(R.id.mapFragment).isDisplayed();
+        onViewWithId(R.id.spinner).isDisplayed();
+    }
+
+    @Then("^I am not parked$")
+    public void i_am_not_parked() {
+        Assert.assertNull(UsersManager.INSTANCE.getCurrentUser().getSpotParked());
+    }
+
+    @Then("^The spot is free$")
+    public void the_spot_is_free() {
+        Spot spot = SpotsManager.INSTANCE.getSpotFromId("TestSpot");
+        Assert.assertEquals(0, spot.getStatus());
+    }
+
 
 
 }
